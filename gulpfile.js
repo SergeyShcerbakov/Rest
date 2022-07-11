904747560a47425b7e8e8babf32acafb95210cd9
Your Name <you@example.com>
2022-07-11 08:46:28 +0300

    add file html, scss and etc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,76 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require("gulp-cssnano");
+const imagemin = require("gulp-imagemin");
+const fileinclude = require("gulp-file-include");
+const { series } = require("gulp");
 
-function rebase(){
-    gulp.src('app/*.html')
-    .pipe(gulp.dest('dist'));
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+};
+
+function buildStyles() {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass().on("error", sass.logError))
+    //.pipe(cssnasano())
+    .pipe(gulp.dest('app/css'));
+};
+
+function csstodist() {
+  return gulp
+    .src('app/css/*.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/css/'));
+};
+
+function fontstodist() {
+  return gulp
+    .src('app/fonts/*.*')
+    // .pipe(cssnano())
+    .pipe(gulp.dest('dist/fonts/'));
+};
+
+function htmlInclude() {
+  return gulp
+    .src("app/*.html")
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
+    .pipe(gulp.dest("dist"));
 }
 
-exports.rebase = rebase;
+function imgmin() {
+  return gulp
+    .src('app/imges/**/*.*')
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 80, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(gulp.dest("dist/imges"));
+}
+
+exports.default = defaultTask; // gulp
+exports.scss = buildStyles;
+exports.css = csstodist;
+exports.html = htmlInclude;
+exports.img = imgmin;
+exports.font = fontstodist;
+// exports.includehtml = include;
+
+exports.dev = series(
+  htmlInclude,
+  buildStyles,
+  csstodist,
+  fontstodist,
+  imgmin
+)
